8abe4f84103a110d7e81d459170f4c68c586100b
svitnev.dmitry <svitnev.dmitry@yandex.ru>
2021-11-05 23:04:21 +0300

    Second commit

---
+++ homework_2.js
@@ -0,0 +1,17 @@
+"кот">"код"// false
+"2"+2*"2"// 24
+undefined == null//true
+undefined === null//false
+null == 0//false
+2 > "3"//false
+null - false + true//1
+1 / "l"//nan
+"2" * "3"//6
+4 + 5 + "O"//90
+"l" + 4 + 5//19
+"4" - 2//2
+"4" - "4x"//nan
+null == false//false
+-4 "/ 0 + 1//
+null + 1//1
+undefined + null//nan
